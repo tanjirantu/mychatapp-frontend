b096dd4ac9fee2810bd984f3d5e921de8a032d32
Tanjir Antu <tanjir@dhakai.com>
2022-09-03 11:40:22 +0600

    add search

--- backend/src/modules/user/repository/getUsers.ts
+++ backend/src/modules/user/repository/getUsers.ts
@@ -1,11 +1,23 @@
 import { Request, ResponseToolkit } from "@hapi/hapi";
 import UserModel from "../model";
-import { sendResponse } from "../../../helper";
+import { flattenObject, sendResponse } from "../../../helper";
 
 export default async (request: Request, h: ResponseToolkit) => {
 	try {
-		const users = await UserModel.find({ isDeleted: false });
-		const count = await UserModel.countDocuments({ isDeleted: false });
+		const skip = request.query?.skip ? request.query.skip : 0;
+		const limit = request.query?.limit ? request.query.limit : 10;
+
+		const findQuery = flattenObject({ isDeleted: false });
+		if (request.query.phone) {
+			findQuery["contact.phoneWithDialCode"] = new RegExp(
+				request.query.phone
+			);
+		}
+		const users = await UserModel.find(findQuery);
+		const count = await UserModel.countDocuments(findQuery)
+			.sort({ _id: -1 })
+			.skip(skip)
+			.limit(limit);
 
 		return h
 			.response(sendResponse({ users, count }, 200, "SUCCESS"))
