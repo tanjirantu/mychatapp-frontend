bd76a9b52cafd0b962acb72ac3ff290d46933f34
Tanjir Antu <tanjir@dhakai.com>
2022-09-05 12:09:04 +0600

    fixed room creation issue

--- backend/src/modules/room/repository/createRoom.ts
+++ backend/src/modules/room/repository/createRoom.ts
@@ -3,13 +3,16 @@ import { RoomCreateInput } from "../types";
 import { sendResponse, sendErrorResponse } from "../../../helper";
 import generateRoomUid from "./generateRoomUid";
 import RoomModel from "../model";
+import { AuthUser } from "../../../shared/types/DecodedAuthToken";
 
 export default async (request: Request, h: ResponseToolkit) => {
 	try {
+		const authUser: any = request.auth.credentials;
 		const payload: RoomCreateInput = request.payload as RoomCreateInput;
 		const uid: string = await generateRoomUid();
 		payload.uid = uid;
 
+		payload.users.push(authUser.userUid);
 		const room = await RoomModel.create(payload);
 
 		return h.response(sendResponse(room, 200, "SUCCESS")).code(200);
