5a556e6647f96d6bf85b5d648ba80f8baaf6baca
Tanjir Antu <tanjir@dhakai.com>
2022-09-10 13:05:28 +0600

    fix getMessages issue

--- backend/src/modules/message/repository/getMessages.ts
+++ backend/src/modules/message/repository/getMessages.ts
@@ -4,7 +4,7 @@ import MessageModel from "../model";
 
 export default async (request: Request, h: ResponseToolkit) => {
 	try {
-		const uid: string = request.query.roomUid;
+		const roomUid: string = request.params.roomUid;
 		const authUser = request.auth.credentials;
 
 		let skip = 0;
@@ -13,9 +13,9 @@ export default async (request: Request, h: ResponseToolkit) => {
 		if (request.query.skip) skip = request.query.skip;
 		if (request.query.limit) skip = request.query.limit;
 
-		const messages = await MessageModel.find({ uid });
+		const messages = await MessageModel.find({ roomUid });
 
-		const count = await MessageModel.countDocuments({ uid })
+		const count = await MessageModel.countDocuments({ roomUid })
 			.sort({ _id: -1 })
 			.skip(skip)
 			.limit(limit);
