d4fe4569dfa5882e26d3333bfb12a76fd9aa71f6
Tanjir Antu <tanjir@dhakai.com>
2022-09-10 23:33:10 +0600

    fix user search issue

--- backend/src/modules/user/repository/getUsers.ts
+++ backend/src/modules/user/repository/getUsers.ts
@@ -6,18 +6,24 @@ export default async (request: Request, h: ResponseToolkit) => {
 	try {
 		const skip = request.query?.skip ? request.query.skip : 0;
 		const limit = request.query?.limit ? request.query.limit : 10;
+		const authUser: any = request.auth.credentials;
 
-		const findQuery = flattenObject({ isDeleted: false });
+		const findQuery = flattenObject({
+			isDeleted: false,
+		});
+
+		findQuery["uid"] = { $ne: authUser.userUid };
+
 		if (request.query.phone) {
 			findQuery["contact.phoneWithDialCode"] = new RegExp(
 				request.query.phone
 			);
 		}
-		const users = await UserModel.find(findQuery);
-		const count = await UserModel.countDocuments(findQuery)
+		const users = await UserModel.find(findQuery)
 			.sort({ _id: -1 })
 			.skip(skip)
 			.limit(limit);
+		const count = await UserModel.countDocuments(findQuery);
 
 		return h
 			.response(sendResponse({ users, count }, 200, "SUCCESS"))
