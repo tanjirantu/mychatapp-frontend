229e401b598fd00efa0d3d93593f5797a9b83df2
Tanjir Antu <tanjir@dhakai.com>
2022-09-26 12:42:05 +0600

    remove unused components

--- frontend/src/libs/findPantoneCode.ts
+++
@@ -1,7 +0,0 @@
-import PantoneCodes from '../constants/pantoneColors';
-
-const findPantoneCode = (colorCode: string) => {
-    return PantoneCodes.find((item) => new RegExp(colorCode, 'i').test(item.pantone));
-};
-
-export default findPantoneCode;
--- frontend/src/modules/common/components/CommentBox/ICommentInputProps.ts
+++
@@ -1,4 +0,0 @@
-import { Comment } from '../../types/Comment';
-export interface ICommentInputProps {
-    onCommentPost: (comment: Comment) => void;
-}
